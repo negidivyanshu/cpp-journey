4500c27e1d9804046582c1687682abcee32a6459
Divyanshu <negidivyanshu9656@gmail.com>
2025-09-21 12:30:31 +0530

    performing basic calcutions using c++

---
+++ basics/calculations.cpp
@@ -0,0 +1,19 @@
+// //Calculating Simple Interest
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float p = 5000; //principle amount
+//     float t = 2; //time
+//     float r = 5; //rate
+//     float si = (p*r*t)/100; //formula for SI 
+//     cout<<"Simple interest on 5000rs at 5% for 2yr will be: "<<si<<endl;
+// }
+
+// //calculating Area of a circle
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float x = 4.3; //radius of a circle
+//     float area = 3.14*x*x; //pie*r*r
+//     cout<<area<<endl;
+// }
