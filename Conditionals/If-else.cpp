cb02366503881d8960e1b7e7565bd137cba4ff76
Divyanshu <negidivyanshu9656@gmail.com>
2025-10-06 20:29:26 +0530

    started learning conditionals along with some practice questions

---
+++ Conditionals/If-else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//   int n;
+//   cout<<"Enter a number :";
+//   cin>>n;
+//   if(n%2==0) cout<<n<<" is an even number";
+//   else cout<<n<<" is an odd number";
+// }
+
+
+int main(){
+  int n;
+  cout<<"Enter an integer :";
+  cin>>n;
+  if(n>0) cout<<n;
+  else cout<<-n;
+}
