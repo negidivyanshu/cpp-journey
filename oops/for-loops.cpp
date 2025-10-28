669921acd9a0152eaf25ac48a0ce041dff732be1
Divyanshu <negidivyanshu9656@gmail.com>
2025-10-28 12:26:39 +0530

    Understanding the use of loops in a programing language

---
+++ oops/for-loops.cpp
@@ -0,0 +1,29 @@
+// printing hello world 10 times
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+//     cout<<"Hello world"<<endl;
+// }
+
+// now  using for loop
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n ; 
+    cout<<"enter n:";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<"Hello World"<<endl;
+    }   
+}
